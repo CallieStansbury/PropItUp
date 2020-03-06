617533459eca5d920daadef65315453ba82c6b58
Callie Stansbury <calliestansbury@gmail.com>
2020-03-06 10:49:21 -0700

    third commit-big inversion

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,27 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class PersonCard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            ageCount: props.Age
-        }
-    }
-    handleClick = () => {
-        this.setState({
-            ageCount: this.state.ageCount + 1
-        })
-    }
-    render() {
-        const { FirstName, LastName, HairColor } = this.props;
-        return (
+const PersonCard = props => {
+    return (
             <div>
-                <h1>{LastName}, {FirstName}</h1>
-                <h4>Age: {this.state.ageCount}</h4>
-                <h4>Hair Color: {HairColor}</h4>
-                <button onClick={this.handleClick}>Birthday for {FirstName} {LastName}</button>
+                <h1>{props.LastName}, {props.FirstName}</h1>
+                <h4>Age: {props.Age}</h4>
+                <h4>Hair Color: {props.HairColor}</h4>
             </div>
         );
     }
-}
 export default PersonCard;
