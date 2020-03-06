02b607d187c73dea6177f16adf1bfb09ae332c8c
Callie Stansbury <calliestansbury@gmail.com>
2020-03-05 17:26:09 -0700

    second commit-putting it all together

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import PersonCard from './components/PersonCard';
 
+
 function App() {
   return (
     <div className="App">
--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+
 class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ageCount: props.Age
+        }
+    }
+    handleClick = () => {
+        this.setState({
+            ageCount: this.state.ageCount + 1
+        })
+    }
     render() {
-        const { FirstName, LastName, Age, HairColor } = this.props;
+        const { FirstName, LastName, HairColor } = this.props;
         return (
             <div>
                 <h1>{LastName}, {FirstName}</h1>
-                <h4>Age: {Age}</h4>
+                <h4>Age: {this.state.ageCount}</h4>
                 <h4>Hair Color: {HairColor}</h4>
+                <button onClick={this.handleClick}>Birthday for {FirstName} {LastName}</button>
             </div>
-        )
+        );
     }
 }
 export default PersonCard;
